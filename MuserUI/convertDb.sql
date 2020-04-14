c61d8e8e5eaba70065278587923068e553d03a4f
Александров Павел Евгеньевич <paul@kontur>
2020-04-14 19:44:58 +0400

    convertdb

---
+++ MuserUI/convertDb.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE IF NOT EXISTS 
+    keyvalues
+    (
+        id    uuid           not null
+                primary key,
+        key   varchar(10000) not null,
+        value varchar(10000) not null
+    );
