05c88e39fc8e70825d609f4de2e99f8c5d5b1b11
tolltech <alexandrovpe@gmail.com>
2020-04-19 18:12:33 +0400

    authenticaion works

--- MuserUI/Controllers/AccountController.cs
+++ MuserUI/Controllers/AccountController.cs
@@ -6,43 +6,52 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Tolltech.MuserUI.Authentications;
+using Tolltech.MuserUI.Models;
 
 namespace Tolltech.MuserUI.Controllers
 {
     public class AccountController : BaseController
     {
         private UserContext db;
+
         public AccountController(UserContext context)
         {
             db = context;
         }
+
         [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel model)
         {
             if (ModelState.IsValid)
             {
-                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+                User user = await db.Users.FirstOrDefaultAsync(u =>
+                    u.Email == model.Email && u.Password == model.Password);
                 if (user != null)
                 {
-                    await Authenticate(model.Email); // аутентификация
+                    await Authenticate(model.Email);
 
                     return RedirectToAction("Index", "Home");
                 }
+
                 ModelState.AddModelError("", "Некорректные логин и(или) пароль");
             }
+
             return View(model);
         }
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterModel model)
@@ -53,7 +62,7 @@ namespace Tolltech.MuserUI.Controllers
                 if (user == null)
                 {
                     // добавляем пользователя в бд
-                    db.Users.Add(new User { Email = model.Email, Password = model.Password });
+                    db.Users.Add(new User {Email = model.Email, Password = model.Password});
                     await db.SaveChangesAsync();
 
                     await Authenticate(model.Email); // аутентификация
@@ -63,6 +72,7 @@ namespace Tolltech.MuserUI.Controllers
                 else
                     ModelState.AddModelError("", "Некорректные логин и(или) пароль");
             }
+
             return View(model);
         }
 
@@ -74,7 +84,8 @@ namespace Tolltech.MuserUI.Controllers
                 new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
             };
             // создаем объект ClaimsIdentity
-            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
             // установка аутентификационных куки
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
         }
