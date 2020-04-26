a164285633f256bd839f0c6760ee2d43d0b5c155
tolltech <alexandrovpe@gmail.com>
2020-04-26 15:18:04 +0400

    some tests migrated

---
+++ TestsNetCore/Domain/VkTrackTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using Tolltech.Muser.Models;
+
+namespace Tolltech.TestsNetCore.Domain
+{
+    public class VkTrackTest : TestBase
+    {
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void TestArtists(string artist, string[] expected)
+        {
+            CollectionAssert.AreEqual(expected, new VkTrack {Artist = artist}.Artists);
+            CollectionAssert.AreEqual(expected, new VkTrack {Artist = artist}.NormalizedArtists);
+        }
+
+        [Test, TestCaseSource(nameof(TestCasesForNormalize))]
+        public void TestWithNormalizedArtists(string artist, string[] expected)
+        {
+            CollectionAssert.AreEqual(expected, new VkTrack {Artist = artist}.AllArtists);
+        }
+
+        private static readonly object[] TestCases =
+        {
+            new object[] {"", Array.Empty<string>()},
+            new object[] {"art", new[] {"art"}},
+            new object[] {"art&art2", new[] {"art", "art2"}},
+            new object[] {"art ft art2", new[] {"art", "art2"}},
+            new object[] {"art ft. art2", new[] {"art", "art2"}},
+            new object[] {"art ft.art2", new[] {"art", "art2"}},
+            new object[] {"art ft.art2", new[] {"art", "art2"}},
+            new object[] {"art,art2,art3", new[] {"art", "art2", "art3"}},
+        };
+
+        private static readonly object[] TestCasesForNormalize =
+        {
+            new object[] {"aRt ft. art2(lyric)", new[] {"art", "art2", "aRt", "art2(lyric)" } },
+            new object[] {"aRt ft. art2<lyric>", new[] {"art", "art2", "aRt", "art2<lyric>" } },
+            new object[] {"art[text] ft.art2", new[] {"art", "art2", "art[text]" } },
+            new object[] {"[text]", new[] {"[text]"}},
+            new object[] {"[text] (lyrics)", new[] {"[text] (lyrics)"}},
+            new object[] {"text (lyrics)", new[] {"text", "text (lyrics)" } },
+        };
+    }
+}
