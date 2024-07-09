d9a144776be123678b120761ff4bc3a65e030d19
tolltech <alexandrovpe@gmail.com>
2024-07-09 15:11:34 +0400

    try to fix pg timestamp lecaxy issue

--- MuserUI/Startup.cs
+++ MuserUI/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,8 @@ namespace Tolltech.MuserUI
             services.ConfigureCors();
 
             services.AddControllersWithViews();
+            
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
